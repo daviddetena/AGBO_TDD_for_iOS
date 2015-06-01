e3a1c9d8e56601891b10abd425b1abda9c252d5c
David de Tena <daviddetenacalvo@gmail.com>
2015-06-01 22:10:03 +0200

    Initial Commit

---
+++ Wallet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wallet
+//
+//  Created by David de Tena on 01/06/15.
+//  Copyright (c) 2015 David de Tena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Wallet/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Wallet
+//
+//  Created by David de Tena on 01/06/15.
+//  Copyright (c) 2015 David de Tena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
